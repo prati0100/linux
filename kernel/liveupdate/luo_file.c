c09c54d7bf2df66dc289932356ea6182fafbf88e
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-11-15 18:27:57 -0500

    tests/liveupdate: Add in-kernel liveupdate test
    Introduce an in-kernel test module to validate the core logic of the
    Live Update Orchestrator's File-Lifecycle-Bound feature. This
    provides a low-level, controlled environment to test FLB registration
    and callback invocation without requiring userspace interaction or
    actual kexec reboots.
    
    The test is enabled by the CONFIG_LIVEUPDATE_TEST Kconfig option.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>

--- kernel/liveupdate/luo_file.c
+++ kernel/liveupdate/luo_file.c
@@ -842,6 +842,8 @@ int liveupdate_register_file_handler(struct liveupdate_file_handler *fh)
 	list_add_tail(&ACCESS_PRIVATE(fh, list), &luo_file_handler_list);
 	luo_session_resume();
 
+	liveupdate_test_register(fh);
+
 	return 0;
 
 out_resume:
@@ -875,8 +877,10 @@ int liveupdate_unregister_file_handler(struct liveupdate_file_handler *fh)
 	if (!liveupdate_enabled())
 		return -EOPNOTSUPP;
 
+	liveupdate_test_unregister(fh);
+
 	if (!luo_session_quiesce())
-		return -EBUSY;
+		goto err_register;
 
 	if (!list_empty(&ACCESS_PRIVATE(fh, flb_list)))
 		goto err_resume;
@@ -889,6 +893,8 @@ int liveupdate_unregister_file_handler(struct liveupdate_file_handler *fh)
 
 err_resume:
 	luo_session_resume();
+err_register:
+	liveupdate_test_register(fh);
 	return err;
 }
 
