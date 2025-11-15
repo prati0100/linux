e02ebbb55a86c0d3068881b68004904273bc7e55
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-11-15 18:26:25 -0500

    liveupdate: luo_session: add sessions support
    Introduce concept of "Live Update Sessions" within the LUO framework.
    LUO sessions provide a mechanism to group and manage `struct file *`
    instances (representing file descriptors) that need to be preserved
    across a kexec-based live update.
    
    Each session is identified by a unique name and acts as a container
    for file objects whose state is critical to a userspace workload, such
    as a virtual machine or a high-performance database, aiming to maintain
    their functionality across a kernel transition.
    
    This groundwork establishes the framework for preserving file-backed
    state across kernel updates, with the actual file data preservation
    mechanisms to be implemented in subsequent patches.
    
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>

--- kernel/liveupdate/luo_internal.h
+++ kernel/liveupdate/luo_internal.h
@@ -19,4 +19,44 @@
  */
 #define luo_restore_fail(__fmt, ...) panic(__fmt, ##__VA_ARGS__)
 
+/**
+ * struct luo_session - Represents an active or incoming Live Update session.
+ * @name:       A unique name for this session, used for identification and
+ *              retrieval.
+ * @files_list: An ordered list of files associated with this session, it is
+ *              ordered by preservation time.
+ * @ser:        Pointer to the serialized data for this session.
+ * @count:      A counter tracking the number of files currently stored in the
+ *              @files_list for this session.
+ * @list:       A list_head member used to link this session into a global list
+ *              of either outgoing (to be preserved) or incoming (restored from
+ *              previous kernel) sessions.
+ * @retrieved:  A boolean flag indicating whether this session has been
+ *              retrieved by a consumer in the new kernel.
+ * @mutex:      Session lock, protects files_list, and count.
+ * @files:      The physically contiguous memory block that holds the serialized
+ *              state of files.
+ * @pgcnt:      The number of pages @files occupy.
+ */
+struct luo_session {
+	char name[LIVEUPDATE_SESSION_NAME_LENGTH];
+	struct list_head files_list;
+	struct luo_session_ser *ser;
+	long count;
+	struct list_head list;
+	bool retrieved;
+	struct mutex mutex;
+	struct luo_file_ser *files;
+	u64 pgcnt;
+};
+
+int luo_session_create(const char *name, struct file **filep);
+int luo_session_retrieve(const char *name, struct file **filep);
+int __init luo_session_setup_outgoing(void *fdt);
+int __init luo_session_setup_incoming(void *fdt);
+int luo_session_serialize(void);
+int luo_session_deserialize(void);
+bool luo_session_quiesce(void);
+void luo_session_resume(void);
+
 #endif /* _LINUX_LUO_INTERNAL_H */
