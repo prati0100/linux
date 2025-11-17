738c98704aa6a09bea7050a80a8bb59000c23915
Pasha Tatashin <pasha.tatashin@soleen.com>
2025-11-17 13:56:23 -0500

    liveupdate: luo_file: add private argument to store runtime state
    Currently file handlers only get the serialized_data field to store
    their state. This field has a pointer to the serialized state of the
    file, and it becomes a part of LUO file's serialized state.
    
    File handlers can also need some runtime state to track information that
    shouldn't make it in the serialized data.
    
    One such example is a vmalloc pointer. While kho_preserve_vmalloc()
    preserves the memory backing a vmalloc allocation, it does not store the
    original vmap pointer, since that has no use being passed to the next
    kernel. The pointer is needed to free the memory in case the file is
    unpreserved.
    
    Provide a private field in struct luo_file and pass it to all the
    callbacks. The field's can be set by preserve, and must be freed by
    unpreserve.
    
    Signed-off-by: Pratyush Yadav <pratyush@kernel.org>
    Co-developed-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Signed-off-by: Pasha Tatashin <pasha.tatashin@soleen.com>
    Reviewed-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- kernel/liveupdate/luo_file.c
+++ kernel/liveupdate/luo_file.c
@@ -128,6 +128,10 @@ static LIST_HEAD(luo_file_handler_list);
  *                 This handle is passed back to the handler's .freeze(),
  *                 .retrieve(), and .finish() callbacks, allowing it to track
  *                 and update its serialized state across phases.
+ * @private_data:  Pointer to the private data for the file used to hold runtime
+ *                 state that is not preserved. Set by the handler's .preserve()
+ *                 callback, and must be freed in the handler's .unpreserve()
+ *                 callback.
  * @retrieved:     A flag indicating whether a user/kernel in the new kernel has
  *                 successfully called retrieve() on this file. This prevents
  *                 multiple retrieval attempts.
@@ -154,6 +158,7 @@ struct luo_file {
 	struct liveupdate_file_handler *fh;
 	struct file *file;
 	u64 serialized_data;
+	void *private_data;
 	bool retrieved;
 	struct mutex mutex;
 	struct list_head list;
@@ -306,6 +311,7 @@ int luo_preserve_file(struct luo_session *session, u64 token, int fd)
 		goto exit_err;
 	} else {
 		luo_file->serialized_data = args.serialized_data;
+		luo_file->private_data = args.private_data;
 		list_add_tail(&luo_file->list, &session->files_list);
 		session->count++;
 	}
@@ -353,6 +359,7 @@ void luo_file_unpreserve_files(struct luo_session *session)
 		args.session = (struct liveupdate_session *)session;
 		args.file = luo_file->file;
 		args.serialized_data = luo_file->serialized_data;
+		args.private_data = luo_file->private_data;
 		luo_file->fh->ops->unpreserve(&args);
 
 		list_del(&luo_file->list);
@@ -380,6 +387,7 @@ static int luo_file_freeze_one(struct luo_session *session,
 		args.session = (struct liveupdate_session *)session;
 		args.file = luo_file->file;
 		args.serialized_data = luo_file->serialized_data;
+		args.private_data = luo_file->private_data;
 
 		err = luo_file->fh->ops->freeze(&args);
 		if (!err)
@@ -401,6 +409,7 @@ static void luo_file_unfreeze_one(struct luo_session *session,
 		args.session = (struct liveupdate_session *)session;
 		args.file = luo_file->file;
 		args.serialized_data = luo_file->serialized_data;
+		args.private_data = luo_file->private_data;
 
 		luo_file->fh->ops->unfreeze(&args);
 	}
