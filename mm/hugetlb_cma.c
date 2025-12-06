0f2180a7b98e26843a1de4dce8460335f0b4cc4f
Pratyush Yadav <pratyush@kernel.org>
2025-12-06 18:46:04 +0100

    mm: hugetlb: disable CMA if liveupdate is enabled
    Hugetlb live update support does not yet work with CMA. Print a warning
    and disable CMA if the config for live updating hugetlb is enabled, and
    liveupdate is enabled at runtime.
    
    Signed-off-by: Pratyush Yadav <pratyush@kernel.org>

--- mm/hugetlb_cma.c
+++ mm/hugetlb_cma.c
@@ -4,6 +4,7 @@
 #include <linux/cma.h>
 #include <linux/compiler.h>
 #include <linux/mm_inline.h>
+#include <linux/liveupdate.h>
 
 #include <asm/page.h>
 #include <asm/setup.h>
@@ -163,6 +164,12 @@ void __init hugetlb_cma_reserve(void)
 	 */
 	VM_WARN_ON(order <= MAX_PAGE_ORDER);
 	gigantic_page_size = PAGE_SIZE << order;
+
+	if (IS_ENABLED(CONFIG_LIVEUPDATE_HUGETLB) && liveupdate_enabled()) {
+		pr_warn("HugeTLB: CMA not supported with live update. Falling back to pre-allocating pages.\n");
+		hugetlb_cma_size = 0;
+		return;
+	}
 
 	hugetlb_bootmem_set_nodes();
 
