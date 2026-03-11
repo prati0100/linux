20dc7588a0dd2bc81c77dba06795c6223396fd98
Pratyush Yadav (Google) <pratyush@kernel.org>
2026-03-11 12:00:37 +0000

    kho: store incoming radix tree in kho_in
    kho_populate() fetches the memory map address to validate if there are
    any incoming preservations. kho_mem_retrieve() duplicates that logic,
    and even happens to miss the fact that the pointer might not be properly
    aligned.
    
    Store the radix tree in kho_in, and initialize it during kho_populate().
    This reduces code duplication, fixes the unaligned access, and allows
    for other callers to access the radix tree while KHO is in scratch-only
    mode.
    
    Signed-off-by: Pratyush Yadav (Google) <pratyush@kernel.org>

--- kernel/liveupdate/kexec_handover.c
+++ kernel/liveupdate/kexec_handover.c
@@ -1261,6 +1261,7 @@ struct kho_in {
 	phys_addr_t fdt_phys;
 	phys_addr_t scratch_phys;
 	struct kho_debugfs dbg;
+	struct kho_radix_tree radix_tree;
 };
 
 static struct kho_in kho_in = {
@@ -1329,24 +1330,8 @@ EXPORT_SYMBOL_GPL(kho_retrieve_subtree);
 
 static int __init kho_mem_retrieve(const void *fdt)
 {
-	struct kho_radix_tree tree;
-	const phys_addr_t *mem;
-	int len;
-
-	/* Retrieve the KHO radix tree from passed-in FDT. */
-	mem = fdt_getprop(fdt, 0, KHO_FDT_MEMORY_MAP_PROP_NAME, &len);
-
-	if (!mem || len != sizeof(*mem)) {
-		pr_err("failed to get preserved KHO memory tree\n");
-		return -ENOENT;
-	}
-
-	if (!*mem)
-		return -EINVAL;
-
-	tree.root = phys_to_virt(*mem);
-	mutex_init(&tree.lock);
-	return kho_radix_walk_tree(&tree, kho_preserved_memory_reserve);
+	return kho_radix_walk_tree(&kho_in.radix_tree,
+				   kho_preserved_memory_reserve);
 }
 
 static __init int kho_out_fdt_setup(void)
@@ -1483,8 +1468,10 @@ void __init kho_memory_init(void)
 		kho_scratch = phys_to_virt(kho_in.scratch_phys);
 		kho_release_scratch();
 
-		if (kho_mem_retrieve(kho_get_fdt()))
+		if (kho_mem_retrieve(kho_get_fdt())) {
 			kho_in.fdt_phys = 0;
+			kho_in.radix_tree.root = NULL;
+		}
 	} else {
 		kho_reserve_scratch();
 	}
@@ -1562,6 +1549,8 @@ void __init kho_populate(phys_addr_t fdt_phys, u64 fdt_len,
 
 	kho_in.fdt_phys = fdt_phys;
 	kho_in.scratch_phys = scratch_phys;
+	kho_in.radix_tree.root = phys_to_virt(mem_map_phys);
+	mutex_init(&kho_in.radix_tree.lock);
 	kho_scratch_cnt = scratch_cnt;
 
 	populated = true;
